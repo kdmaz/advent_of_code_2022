bfd311aeaae0f9f3ecc134c46bb1e53356dbbb15
Kyle Mazeika <kyle.mazeika@gmail.com>
2022-12-03 10:49:19 -0500

    use iter methods

--- src/solutions/day03/day03_part2.rs
+++ src/solutions/day03/day03_part2.rs
@@ -3,25 +3,27 @@ use std::collections::HashSet;
 
 fn main(input: &str) -> i32 {
     let lines: Vec<&str> = input.lines().collect();
-    let mut sum = 0;
 
-    for i in (0..lines.len()).step_by(3) {
-        let mut groups = [HashSet::new(), HashSet::new(), HashSet::new()];
+    (0..lines.len())
+        .step_by(3)
+        .map(|i| {
+            let mut backpacks = [HashSet::new(), HashSet::new(), HashSet::new()];
 
-        for (j, line) in lines.iter().skip(i).enumerate().take(3) {
-            for c in line.chars() {
-                groups[j].insert(c);
+            for (j, line) in lines.iter().skip(i).enumerate().take(3) {
+                for c in line.chars() {
+                    backpacks[j].insert(c);
+                }
             }
-        }
 
-        for c in &groups[0] {
-            if groups.iter().all(|group| group.contains(c)) {
-                sum += get_priority(*c);
+            for c in &backpacks[0] {
+                if backpacks.iter().all(|backpack| backpack.contains(c)) {
+                    return get_priority(*c);
+                }
             }
-        }
-    }
 
-    sum
+            panic!("Could not find common char in backpacks");
+        })
+        .sum()
 }
 
 #[cfg(test)]
