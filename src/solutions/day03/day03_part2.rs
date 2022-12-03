431c63278dcc058631aaadebb75ee6aa83a2c1a2
Kyle Mazeika <kyle.mazeika@gmail.com>
2022-12-03 10:15:59 -0500

    day03 part2

--- src/solutions/day03/day03_part2.rs
+++ src/solutions/day03/day03_part2.rs
@@ -1,5 +1,32 @@
-fn main(_input: &str) -> i32 {
-    -1
+use std::collections::HashSet;
+
+use super::day03_shared::get_priority;
+
+fn main(input: &str) -> i32 {
+    let mut line_index = 0;
+    let lines: Vec<&str> = input.lines().collect();
+    let mut sum = 0;
+
+    while line_index < lines.len() {
+        let mut groups = [HashSet::new(), HashSet::new(), HashSet::new()];
+
+        for (current_line_index, _) in lines.iter().enumerate().skip(line_index).take(3) {
+            for c in lines[current_line_index].chars() {
+                let group_index = current_line_index - line_index;
+                groups[group_index].insert(c);
+            }
+        }
+
+        for c in &groups[0] {
+            if groups.iter().all(|group| group.contains(c)) {
+                sum += get_priority(*c);
+            }
+        }
+
+        line_index += 3;
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -11,7 +38,7 @@ mod tests {
     fn example() {
         let input = read_file("examples", 3);
         let output = main(&input);
-        let expected = 0;
+        let expected = 70;
         assert_eq!(output, expected);
     }
 
@@ -19,7 +46,7 @@ mod tests {
     fn input() {
         let input = read_file("input", 3);
         let output = main(&input);
-        let expected = 0;
+        let expected = 2683;
         assert_eq!(output, expected);
     }
 }
