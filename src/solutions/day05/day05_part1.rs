307a1a5ced9fbeb578fb23fa6be1af45ba06c972
Kyle Mazeika <kyle.mazeika@gmail.com>
2022-12-05 23:13:15 -0500

    solve day05 part1

--- src/solutions/day05/day05_part1.rs
+++ src/solutions/day05/day05_part1.rs
@@ -1,32 +1,70 @@
 use std::str::FromStr;
 
+#[derive(Debug)]
 struct Procedure {
     stacks: Vec<Vec<char>>,
     steps: Vec<Step>,
 }
 
 impl Procedure {
+    fn build(input: &str) -> Self {
+        let mut procedure = Procedure {
+            stacks: vec![],
+            steps: vec![],
+        };
+
+        for line in input.lines() {
+            if line.starts_with('m') {
+                let step = Step::from_str(line).unwrap();
+                procedure.steps.push(step);
+                continue;
+            }
+
+            let mut is_crate = false;
+            for (i, c) in line.chars().enumerate() {
+                let stack_i = i / 4;
+
+                if procedure.stacks.len() == stack_i {
+                    procedure.stacks.push(vec![]);
+                }
+
+                if is_crate {
+                    procedure.stacks[stack_i].push(c);
+                    is_crate = false;
+                }
+
+                if c == '[' {
+                    is_crate = true;
+                }
+            }
+        }
+
+        procedure
+    }
+
     fn run(&mut self) -> String {
+        for stack in &mut self.stacks {
+            stack.reverse();
+        }
+
         for step in &self.steps {
             let Step { qty, from, to } = step;
             for _ in 0..*qty {
-                let cargo = self.stacks[from - 1].pop().unwrap();
-                self.stacks[to - 1].push(cargo);
+                let c = self.stacks[from - 1].pop().unwrap();
+                self.stacks[to - 1].push(c);
             }
         }
 
         let mut result = String::new();
-
         for stack in &mut self.stacks {
-            let cargo = stack.pop().unwrap();
-            result.push(cargo);
+            let c = stack.pop().unwrap();
+            result.push(c);
         }
-
         result
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct Step {
     qty: u8,
     from: usize,
@@ -44,16 +82,8 @@ impl Step {
     }
 }
 
-#[derive(PartialEq, Eq)]
-enum Code {
-    Move,
-    From,
-    To,
-    Invalid,
-}
-
 impl FromStr for Code {
-    type Err = String;
+    type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(match s {
@@ -66,42 +96,47 @@ impl FromStr for Code {
 }
 
 impl FromStr for Step {
-    type Err = String;
+    type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut step = Step::default();
         let mut code_buffer = String::new();
         let mut val_buffer = String::new();
+        let chars: Vec<char> = s.chars().collect();
+        let len = chars.len();
 
-        for char in s.chars() {
+        for (i, char) in chars.into_iter().enumerate() {
             let code = Code::from_str(&code_buffer).unwrap();
             if code == Code::Invalid {
                 code_buffer.push(char);
                 continue;
             }
 
-            if char == ' ' {
-                let val = val_buffer.parse::<u8>().unwrap();
+            val_buffer.push(char);
+
+            if char == ' ' || i + 1 == len {
+                let val = val_buffer.trim().parse::<u8>().unwrap();
                 step.update(code, val);
                 code_buffer.clear();
                 val_buffer.clear();
                 continue;
             }
-
-            val_buffer.push(char);
         }
 
         Ok(step)
     }
 }
 
-fn main(input: &str) -> String {
-    for line in input.lines() {
-        let i = 0;
-        while i < line.len() {}
-    }
+#[derive(PartialEq, Eq, Debug)]
+enum Code {
+    Move,
+    From,
+    To,
+    Invalid,
+}
 
-    "".to_owned()
+fn main(input: &str) -> String {
+    Procedure::build(input).run()
 }
 
 #[cfg(test)]
@@ -121,7 +156,7 @@ mod tests {
     fn input() {
         let input = read_file("input", 5);
         let output = main(&input);
-        let expected = "".to_owned();
+        let expected = "VWLCWGSDQ".to_owned();
         assert_eq!(output, expected);
     }
 }
