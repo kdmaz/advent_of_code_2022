e6fdb0de72b3a4b6065d02e8d319555b0bfe0228
Kyle Mazeika <kyle.mazeika@gmail.com>
2022-12-10 11:27:00 -0500

    simplify day9 part1

--- src/solutions/day09/day09_part1.rs
+++ src/solutions/day09/day09_part1.rs
@@ -1,189 +1,68 @@
-use std::{cmp, str::FromStr};
+use std::collections::HashSet;
 
-#[derive(Debug)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
 struct Position {
     x: i32,
     y: i32,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Direction {
-    U,
-    D,
-    L,
-    R,
-}
+fn change_position(direction: &str, head: &mut Position, tail: &mut Position) {
+    match direction {
+        "U" => {
+            head.y += 1;
 
-impl FromStr for Direction {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(match s {
-            "U" => Direction::U,
-            "D" => Direction::D,
-            "R" => Direction::R,
-            "L" => Direction::L,
-            _ => panic!("Invalid direction"),
-        })
-    }
-}
-
-#[derive(Debug)]
-struct Motion {
-    direction: Direction,
-    distance: i32,
-}
-
-#[derive(Debug)]
-struct Bridge {
-    grid: Vec<Vec<String>>,
-    start: Position,
-    motions: Vec<Motion>,
-}
-
-impl Bridge {
-    fn simulate_rope(&mut self) {
-        let Bridge {
-            grid,
-            start,
-            motions,
-        } = self;
-        let mut head = Position {
-            x: start.x,
-            y: start.y,
-        };
-        let mut tail = Position {
-            x: start.x,
-            y: start.y,
-        };
-
-        grid[start.y as usize][start.x as usize] = "#".to_owned();
-
-        for Motion {
-            direction,
-            distance,
-        } in motions
-        {
-            for _ in 0..*distance {
-                match *direction {
-                    Direction::U => {
-                        head.y += 1;
-
-                        if head.y - tail.y == 2 {
-                            tail.y = head.y - 1;
-
-                            if tail.x != head.x {
-                                tail.x = head.x;
-                            }
-                        }
-                    }
-                    Direction::D => {
-                        head.y -= 1;
-
-                        if tail.y - head.y == 2 {
-                            tail.y = head.y + 1;
-
-                            if tail.x != head.x {
-                                tail.x = head.x;
-                            }
-                        }
-                    }
-                    Direction::R => {
-                        head.x += 1;
-
-                        if head.x - tail.x == 2 {
-                            tail.x = head.x - 1;
-
-                            if tail.y != head.y {
-                                tail.y = head.y;
-                            }
-                        }
-                    }
-                    Direction::L => {
-                        head.x -= 1;
-
-                        if tail.x - head.x == 2 {
-                            tail.x = head.x + 1;
-
-                            if tail.y != head.y {
-                                tail.y = head.y;
-                            }
-                        }
-                    }
-                }
-
-                grid[tail.y as usize][tail.x as usize] = "#".to_owned();
+            if head.y.abs_diff(tail.y) == 2 {
+                tail.y = head.y - 1;
+                tail.x = head.x;
             }
         }
-    }
+        "D" => {
+            head.y -= 1;
 
-    fn count_visited(&self) -> i32 {
-        self.grid
-            .iter()
-            .map(|row| {
-                row.iter()
-                    .map(|col| i32::from(col.as_str() == "#"))
-                    .sum::<i32>()
-            })
-            .sum()
-    }
-}
-
-impl FromStr for Bridge {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut min = Position { x: 0, y: 0 };
-        let mut max = Position { x: 0, y: 0 };
-        let mut current = Position { x: 0, y: 0 };
-        let mut start = Position { x: 0, y: 0 };
-        let mut motions = vec![];
-
-        for line in s.lines() {
-            let parts: Vec<&str> = line.split(' ').collect();
-            let direction = Direction::from_str(parts[0]).unwrap();
-            let distance = parts[1].parse().unwrap();
-            let motion = Motion {
-                direction,
-                distance,
-            };
-            motions.push(motion);
-
-            match direction {
-                Direction::R => current.x += distance,
-                Direction::L => current.x -= distance,
-                Direction::U => current.y += distance,
-                Direction::D => current.y -= distance,
-            };
-
-            min.x = cmp::min(min.x, current.x);
-            min.y = cmp::min(min.y, current.y);
-
-            max.x = cmp::max(max.x, current.x);
-            max.y = cmp::max(max.y, current.y);
-
-            start.x = cmp::min(min.x, start.x).abs();
-            start.y = cmp::min(min.y, start.y).abs();
+            if head.y.abs_diff(tail.y) == 2 {
+                tail.y = head.y + 1;
+                tail.x = head.x;
+            }
         }
+        "R" => {
+            head.x += 1;
 
-        let length = (max.x - min.x) + 1;
-        let height = (max.y - min.y) + 1;
+            if head.x.abs_diff(tail.x) == 2 {
+                tail.x = head.x - 1;
+                tail.y = head.y;
+            }
+        }
+        "L" => {
+            head.x -= 1;
 
-        let row = vec![".".to_owned(); length as usize];
-        let grid = vec![row; height as usize];
-
-        Ok(Bridge {
-            grid,
-            start,
-            motions,
-        })
+            if head.x.abs_diff(tail.x) == 2 {
+                tail.x = head.x + 1;
+                tail.y = head.y;
+            }
+        }
+        _ => {}
     }
 }
 
 fn main(input: &str) -> i32 {
-    let mut grid = Bridge::from_str(input).unwrap();
-    grid.simulate_rope();
-    grid.count_visited()
+    let start = Position { x: 0, y: 0 };
+    let mut head = start;
+    let mut tail = start;
+    let mut visited_positions = HashSet::new();
+    visited_positions.insert(start);
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split(' ').collect();
+        let direction = parts[0];
+        let distance: i32 = parts[1].parse().unwrap();
+
+        for _ in 0..distance {
+            change_position(direction, &mut head, &mut tail);
+            visited_positions.insert(tail);
+        }
+    }
+
+    visited_positions.len() as i32
 }
 
 #[cfg(test)]
